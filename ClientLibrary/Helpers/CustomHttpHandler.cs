12942a10f214c9d4d8cf38db83390a2aa554fc59
Berkay Huz <132297685+berkayhuz@users.noreply.github.com>
2024-02-05 08:57:09 +0300

    go

---
+++ ClientLibrary/Helpers/CustomHttpHandler.cs
@@ -0,0 +1,58 @@
+﻿using BaseLibrary.DTOs;
+using ClientLibrary.Services.Contracts;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClientLibrary.Helpers
+{
+    public class CustomHttpHandler (GetHttpClient getHttpClient, LocalStorageService localStorageService, IUserAccountService accountService) : DelegatingHandler
+    {
+		protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+		{
+			bool loginUrl = request.RequestUri!.AbsoluteUri.Contains("login");
+            bool registerUrl = request.RequestUri!.AbsoluteUri.Contains("register");
+            bool refreshTokenUrl = request.RequestUri!.AbsoluteUri.Contains("refresh-token");
+            
+            if (loginUrl || registerUrl || refreshTokenUrl) return await base.SendAsync(request, cancellationToken);
+
+            var result = await base.SendAsync(request, cancellationToken);
+
+            //localstorage den token al
+            var stringToken = await localStorageService.GetToken();
+            if (stringToken != null) return result;
+
+            // header container tokenleri kontrol et
+            if (result.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                string token = string.Empty;
+                try { token = request.Headers.Authorization!.Parameter!; }
+                catch { }
+
+                var deserializedToken = Serializations.DeserializeJsonString<UserSession>(stringToken);
+                if (deserializedToken is null) return result;
+
+                if (string.IsNullOrEmpty(token))
+                {
+                    request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", deserializedToken.Token);
+                    return await base.SendAsync(request, cancellationToken);
+                }
+
+                // refresh token çağır
+                var newJwtToken = await GetRefreshToken(deserializedToken.RefreshToken!);
+                if (string.IsNullOrEmpty(newJwtToken)) return result;
+
+                request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", newJwtToken);
+                    return await base.SendAsync(request, cancellationToken);
+            }
+            return result;
+        }
+
+        private async Task<string> GetRefreshToken(string refreshToken)
+        {
+            var result = await accountService.RefreshTokenAsync(new RefreshToken() { Token = refreshToken});
+            string serializedToken = Serializations.SerializeObj(new UserSession()
+            { Token = result.Token, RefreshToken = result.RefreshToken });
+            await localStorageService.SetToken(serializedToken);
+            return result.Token;
+        }
+    }
+}
